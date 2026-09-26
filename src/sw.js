d789306fa73dfad8eb113dfc9b6156734cd79365
Saurabh Verma <saurabhv749@gmail.com>
2026-09-26 23:33:50 +0530

    skip peerjs requests

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v2-1-1"
+const CACHE_VERSION = "v2-1-2"
 const CACHE_NAME = `finance-tracker-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `app-runtime-${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
@@ -169,7 +169,7 @@ self.addEventListener('fetch', (event) => {
     }
 
     // Skip peerjs requests
-    if (!url.protocol.includes('peerjs')) {
+    if (url.protocol.includes('peerjs')) {
         return;
     }
 
